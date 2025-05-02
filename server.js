ca6bd9daff800573621a897b360ea7d13c4b85c1
ninemui <ninemui@gmail.com>
2025-05-01 20:03:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -113,12 +113,12 @@ app.get('/search', async (req, res) => {
 });
 
 // Server setup
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => 
     console.log(`Server running on http://localhost:${PORT}`)
 );
 
-// Set server timeout to 8 seconds (10000 ms)
+// Set server timeout to 10 seconds (10000 ms)
 server.setTimeout(10000, (socket) => {
     console.log("Request timed out");
     socket.end('HTTP/1.1 408 Request Timeout\r\n\r\n'); // Send timeout response
