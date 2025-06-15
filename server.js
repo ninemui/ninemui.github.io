74fd67bb93572d5a75e767fb4a51c3f1051877e7
ninemui <ninemui@gmail.com>
2025-06-14 23:08:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const cors = require('cors');
 const path = require('path');
 const { setTimeout } = require('timers/promises');
 
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+  'Accept-Language': 'en-US,en;q=0.9',
+  'Referer': 'https://www.google.com/',
+  'Sec-Fetch-Dest': 'document',
+  'Sec-Fetch-Mode': 'navigate',
+  'Sec-Fetch-Site': 'cross-site',
+  'Sec-Fetch-User': '?1',
+  'Upgrade-Insecure-Requests': '1'
+};
+
 const app = express();
 app.use(cors());
 
@@ -40,10 +52,7 @@ const scrapePage = async (url, baseUrl, selector, query) => {
         
         const response = await Promise.race([
             axios.get(fullUrl, {
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-                    "Accept-Language": "en-US,en;q=0.9",
-                },
+                headers: headers,
                 signal: controller.signal
             }),
             setTimeout(4000).then(() => {
